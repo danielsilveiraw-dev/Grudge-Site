e088489ca6558e6fafbaa8d1b85fa162eeb7baac
Daniel silveira <danielsilveiraw@gmail.co>
2026-08-13 01:59:38 -0300

    Remoção de palavras

--- components/HomeAtmosphere.tsx
+++ components/HomeAtmosphere.tsx
@@ -73,31 +73,6 @@ export default function HomeAtmosphere() {
       {/* GLOW INFERIOR */}
       <div className="absolute bottom-[-280px] left-1/2 h-[600px] w-[950px] -translate-x-1/2 rounded-full bg-accent-hot/[0.055] blur-[180px]" />
 
-      {/* GRUDGE LATERAL */}
-      <div
-        className="absolute left-[-80px] top-1/2 hidden -translate-y-1/2 -rotate-90 select-none 2xl:block"
-        style={{
-          marginTop:
-            `${moveY * 0.6}px`,
-        }}
-      >
-        <span className="font-display text-[6rem] uppercase tracking-[0.38em] text-text-main/[0.045]">
-          GRUDGE
-        </span>
-      </div>
-
-      {/* UNKNOWN LATERAL */}
-      <div
-        className="absolute right-[-125px] top-[58%] hidden rotate-90 select-none 2xl:block"
-        style={{
-          marginTop:
-            `${-moveY * 0.5}px`,
-        }}
-      >
-        <span className="font-display text-[5rem] uppercase tracking-[0.38em] text-accent-hot/[0.05]">
-          UNKNOWN
-        </span>
-      </div>
 
       {/* PONTOS DE LUZ */}
       <div
